ffcde0c8fe281f9880c6967447a6976d83794323
aurelticot <aurelticot.dev@gmail.com>
2025-02-04 15:23:28 +1100

    feat: update title of landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function RootPage() {
   return (
     <RootConnectionHandler>
       <div className="flex h-full w-full flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Landing page when not connected</h1>
+        <h1 className="text-2xl font-bold">Verida: AI Developer Admin</h1>
         <VeridaConnectButton />
       </div>
     </RootConnectionHandler>
