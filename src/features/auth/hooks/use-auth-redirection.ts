cba09a478e726c191e1375ced9fd0fbc9d70ad41
aurelticot <aurelticot.dev@gmail.com>
2025-05-06 21:53:42 +0200

    fix: reenable redirect after auth

--- src/features/auth/hooks/use-auth-redirection.ts
+++ src/features/auth/hooks/use-auth-redirection.ts
@@ -1,30 +1,28 @@
-import { redirect } from "next/navigation"
-import { useCallback } from "react"
+import { redirect, usePathname, useSearchParams } from "next/navigation"
+import { useCallback, useMemo } from "react"
 
+import { useAuthRedirectPathState } from "@/features/auth/hooks/use-auth-redirect-path-state"
 import { getRootPageRoute } from "@/features/routes/utils"
 
 export function useAuthRedirection() {
-  // const { serializeRedirectPath } = useAuthRedirectPathState()
+  const { serializeRedirectPath } = useAuthRedirectPathState()
 
-  // const pathName = usePathname()
-  // const searchParams = useSearchParams()
+  const pathName = usePathname()
+  const searchParams = useSearchParams()
 
-  // const targetUrl = useMemo(
-  //   () =>
-  //     `${pathName}${searchParams.toString() ? `?${searchParams.toString()}` : ""}`,
-  //   [pathName, searchParams]
-  // )
+  const targetUrl = useMemo(
+    () =>
+      `${pathName}${searchParams.toString() ? `?${searchParams.toString()}` : ""}`,
+    [pathName, searchParams]
+  )
 
   const redirectToAuthPage = useCallback(() => {
-    redirect(getRootPageRoute())
-
-    // TODO: Re-enable the redirectPath but handle the disconnect case which doesn't need the redirectPath
-    // redirect(
-    //   serializeRedirectPath(getRootPageRoute(), {
-    //     redirectPath: targetUrl,
-    //   })
-    // )
-  }, [])
+    redirect(
+      serializeRedirectPath(getRootPageRoute(), {
+        redirectPath: targetUrl,
+      })
+    )
+  }, [serializeRedirectPath, targetUrl])
 
   return {
     redirectToAuthPage,
